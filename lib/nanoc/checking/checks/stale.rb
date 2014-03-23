e7edd9e5521fa5842a6d78fa3ad6a144bfb81616
Denis Defreyne <denis.defreyne@stoneship.org>
2014-03-23 11:43:19 +0100

    Implement stale check

--- lib/nanoc/checking/checks/stale.rb
+++ lib/nanoc/checking/checks/stale.rb
@@ -7,16 +7,11 @@ module Nanoc::Checking::Checks
     identifier :stale
 
     def run
-      # FIXME re-enable this
-      # The stale check is broken until the pruner is cleaned up
-      # (merged into ItemRepWriter and put in core)
-      return
-
       item_rep_paths = self.item_rep_paths
 
-      self.output_filenames.each do |f|
-        # FIXME re-enable pruner
-#        next if self.pruner.filename_excluded?(f)
+      self.build_filenames.each do |f|
+        # FIXME filename_excluded? is part of filesystem pruner only
+        next if self.pruner.filename_excluded?(f)
         if !item_rep_paths.include?(f)
           self.add_issue(
             "file without matching item",
@@ -27,25 +22,22 @@ module Nanoc::Checking::Checks
 
   protected
 
+    def compiler
+      @_compiler ||= Nanoc::CompilerBuilder.new.build(@site)
+    end
+
     def item_rep_paths
-      compiler = Nanoc::Compiler.new(@site)
-      compiler.load
-      compiler.build_reps
       compiler.item_rep_store.reps.
-        flat_map { |r| r.paths_without_snapshot }.
+        flat_map { |r| r.written_paths }.
         map { |r| File.join(@site.config[:build_dir], r) }
     end
 
     def pruner
       @pruner ||= begin
         exclude_config = @site.config.fetch(:prune, {}).fetch(:exclude, [])
-        compiler = Nanoc::Compiler.new(self.site)
         identifier = compiler.item_rep_writer.class.identifier
-        # FIXME re-enable pruner
-        #pruner_class = Nanoc::Extra::Pruner.named(identifier)
-        #pruner_class.new(@site, :exclude => exclude_config)
-        # TODO implement
-        nil
+        pruner_class = Nanoc::Pruner.named(identifier)
+        pruner_class.new(@site, :exclude => exclude_config)
       end
     end
 
